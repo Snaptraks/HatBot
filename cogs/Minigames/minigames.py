076036d15b0fdfbfdf5e634e5ac5a5b22773b452
François Hardy <snaptraks@gmail.com>
2019-08-17 15:32:25 -0400

    Add Minigames cog skeleton

---
+++ cogs/Minigames/minigames.py
@@ -0,0 +1,50 @@
+import asyncio
+
+import discord
+from discord.ext import commands
+
+from ..utils.cog import FunCog
+
+class Minigames(FunCog):
+    """Collection of minigames to play!"""
+
+    def __init__(self, bot):
+        super().__init__(bot)
+
+    @commands.command()
+    async def blackjack(self, ctx):
+        # Proposed by Kootiepatra
+        pass
+
+    @commands.command()
+    async def boogle(self, ctx):
+        # Proposed by Kootiepatra
+        pass
+
+    @commands.command()
+    async def connect4(self, ctx):
+        pass
+
+    @commands.command()
+    async def hangman(self, ctx):
+        pass
+
+    @commands.command()
+    async def higherlower(self, ctx):
+        # Proposed by Outerwebs
+        pass
+
+    @commands.command()
+    async def mancala(self, ctx):
+        # Proposed by Princerbang
+        pass
+
+    @commands.command()
+    async def tictactoe(self, ctx):
+        # Proposed by Princerbang
+        pass
+
+    @commands.command()
+    async def wordsearch(self, ctx):
+        # Proposed by Tant
+        pass
