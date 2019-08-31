d67aa4eceef16795ed1e48ca0319c84337d4ba5c
François Hardy <snaptraks@gmail.com>
2019-08-31 15:56:08 -0400

    Make blackjack discord playable

--- cogs/Minigames/blackjack.py
+++ cogs/Minigames/blackjack.py
@@ -1,68 +1,177 @@
 import asyncio
+
+import numpy as np
 import discord
 
-from cards import Deck, Hand
+from .cards import Deck, Hand
+from . import emoji
+
 
 
 class Blackjack:
     def __init__(self, ctx, bot):
+        self.ctx = ctx
+        self.bot = bot
+        # Hit or Stand
+        self.moves = [emoji.Alphabet.H.value, emoji.Alphabet.S.value]
         self.deck = Deck()
         self.deck.shuffle()
+        self.playing = True
 
         self.player_hand = Hand()
         self.dealer_hand = Hand()
         self.deck.give_cards(self.player_hand, 2)
         self.deck.give_cards(self.dealer_hand, 2)
+        self.player_busted = False
+        self.dealer_busted = False
+
+        self.embed = discord.Embed(
+            title=None,
+            type='rich',
+            color=np.random.randint(0xFFFFFF),  # Random color
+            ).set_author(
+            name=self.ctx.author.display_name,
+            icon_url=self.ctx.author.avatar_url_as(static_format='png'),
+            ).add_field(
+            name='Blackjack',
+            value=None,  # will be filled later
+            inline=False,
+            ).add_field(
+            name='Player\'s Hand',
+            value=None,  # will be filled later
+            inline=True,
+            ).add_field(
+            name='Dealer\'s Hand',
+            value=None,  # will be filled after
+            inline=True,
+            ).set_footer(
+            text='Hit or Stand?',
+            )
 
     async def play(self):
+        self.update_embed('Please wait, setting things up...')
+        self.message_game = await self.ctx.send(embed=self.embed)
+
+        def check(reaction, user):
+            valid = user == self.ctx.author and \
+            reaction.message.id == self.message_game.id and \
+            reaction.emoji in self.moves
+
+            return valid
+
+        for move in self.moves:
+            await self.message_game.add_reaction(move)
+
         # display cards?
-        while True:
+        while self.playing:
             print('Player:', self.player_hand.cards)
             print('Dealer:', self.dealer_hand.cards[:-1])
+            hint_message = (
+                'Your score is '
+                f'**{self.calculate_score(self.player_hand)}**.'
+                )
+            self.update_embed(hint_message)
+            await self.message_game.edit(embed=self.embed)
             # check for blackjacks
             if self.calculate_score(self.player_hand) == 21:
                 print('Player got a Blackjack!')
-                return
+                self.playing = False
+                hint_message = 'Congratulations! You got a Blackjack!'
+                continue
             elif self.calculate_score(self.dealer_hand) == 21:
-                print('Dealer got a Blackjack!')
-                return
+                # print('Dealer got a Blackjack!')
+                self.playing = False
+                hint_message = 'Sadly the dealer got a Blackjack...'
+                continue
 
             # ask player input
-            move = input('[H]it or [S]tand? ').upper()
+            try:
+                reaction, user = await self.bot.wait_for(
+                    'reaction_add',
+                    timeout=5 * 60,  # 5 minutes
+                    check=check,
+                    )
+            except asyncio.TimeoutError as e:
+                # should probably stand if timeout
+                print('Timeout')
+                self.playing = False
+                continue
+
+            move = reaction.emoji
+            await reaction.remove(user)
             # if hit
-            if move == 'H':
+            if move == emoji.Alphabet.H.value:
                 # give card
                 self.deck.give_cards(self.player_hand, 1)
                 print(self.player_hand.cards)
                 if self.calculate_score(self.player_hand) > 21:
                     print('Player busted.')
-                    return
+                    self.playing = False
+                    self.player_busted = True
+                    hint_message = (
+                        'You busted with a score of '
+                        f'**{self.calculate_score(self.player_hand)}**.'
+                        )
             # elif stand
-            elif move == 'S':
+            elif move == emoji.Alphabet.S.value:
                 # give dealer card until total is above 17
                 while self.calculate_score(self.dealer_hand) < 17:
                     self.deck.give_cards(self.dealer_hand, 1)
                     print(self.dealer_hand.cards)
                     if self.calculate_score(self.dealer_hand) > 21:
                         print('Dealer busted.')
-                        return
+                        self.playing = False
+                        self.dealer_busted = True
+                        hint_message = (
+                            'The dealer busted with a score of '
+                            f'**{self.calculate_score(self.dealer_hand)}**.'
+                            )
+                        continue
 
                 if self.calculate_score(self.dealer_hand) > \
-                        self.calculate_score(self.player_hand):
+                        self.calculate_score(self.player_hand) and \
+                        not self.dealer_busted:
                     print('Dealer won.')
+                    hint_message = (
+                        'The dealer won! You got '
+                        f'**{self.calculate_score(self.player_hand)}** '
+                        'while the dealer got '
+                        f'**{self.calculate_score(self.dealer_hand)}**.'
+                        )
 
                 elif self.calculate_score(self.dealer_hand) < \
-                        self.calculate_score(self.player_hand):
+                        self.calculate_score(self.player_hand) and \
+                        not self.player_busted:
                     print('Player won.')
+                    hint_message = (
+                        'You won! You got '
+                        f'**{self.calculate_score(self.player_hand)}** '
+                        'while the dealer got '
+                        f'**{self.calculate_score(self.dealer_hand)}**.'
+                        )
 
-                else:
+                elif self.calculate_score(self.dealer_hand) == \
+                        self.calculate_score(self.player_hand):
                     print('Tie')
+                    hint_message = (
+                        'It is a tie! You both got '
+                        f'**{self.calculate_score(self.player_hand)}**.'
+                        )
 
-                return
+                self.playing = False
+
+            self.update_embed(hint_message)
+            await self.message_game.edit(embed=self.embed)
+
+        self.update_embed(hint_message)
+        await self.message_game.edit(embed=self.embed)
+
+        await self.message_game.clear_reactions()
 
     def calculate_score(self, hand):
         hand = hand.cards.copy()
-        hand = sorted(hand, reverse=True)  # put aces at the end
+        # put aces at the end
+        hand = sorted(hand, key=lambda x: x.rank, reverse=True)
         score = 0
         for card in hand:
             if card.rank > 10:
@@ -76,5 +185,33 @@ class Blackjack:
                 score += card.rank
         return score
 
-    def update_embed(self):
-        pass
+    def update_embed(self, hint_message):
+        player_cards = '\n'.join(card.emoji
+            for card in self.player_hand)
+
+        if self.playing:
+            dealer_cards = \
+                [card.emoji for card in self.dealer_hand.cards[:-1]] + \
+                [emoji.Suits.JOKER.value]
+
+        else:
+            dealer_cards = [card.emoji for card in self.dealer_hand]
+
+        dealer_cards = '\n'.join(dealer_cards)
+
+        self.embed.set_field_at(
+            index=0,
+            name=self.embed.fields[0].name,
+            value=hint_message,
+            inline=self.embed.fields[0].inline,
+            ).set_field_at(
+            index=1,  # player's hand
+            name=self.embed.fields[1].name,
+            value=player_cards,
+            inline=self.embed.fields[1].inline,
+            ).set_field_at(
+            index=2,  # dealer's hand
+            name=self.embed.fields[2].name,
+            value=dealer_cards,
+            inline=self.embed.fields[2].inline
+            )
