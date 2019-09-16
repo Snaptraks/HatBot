9935cad64eb9cd71e3d39639ac367bd2bbd3c17d
François Hardy <snaptraks@gmail.com>
2019-09-16 19:27:24 -0400

    Use .listener() for responses

--- cogs/Responses/responses.py
+++ cogs/Responses/responses.py
@@ -23,10 +23,22 @@ class Responses(BasicCog):
 
         # Background tasks
         self.bg_tasks = [
-            self.bot.loop.create_task(self.they_said()),
-            self.bot.loop.create_task(self.hello_there()),
-            self.bot.loop.create_task(self.on_mention()),
             ]
+
+        # hello_there parameters
+        self.hello_there_params = {
+            'cooldown': timedelta(minutes=1),
+            'last_time': datetime.min,
+            }
+
+        # they_said parameters
+        self.they_said_params = {
+            'cooldown': timedelta(minutes=1),
+            'chained': False,
+            'last_author': None,
+            'last_message': None,
+            'last_time': datetime.min,
+            }
 
     def cog_unload(self):
         super().cog_unload()
@@ -43,19 +55,12 @@ class Responses(BasicCog):
 
         self.guild = guild
 
-    async def on_mention(self):
+    @commands.Cog.listener(name='on_message')
+    async def on_mention(self, message):
         """Sends a funny reply when the bot is mentionned."""
-        await self.bot.wait_until_ready()
 
-        def check(message):
-            content = message.content
-            valid = self.bot.user.mention in content and \
-                not content.startswith(self.bot.command_prefix)
-            return valid
-
-        while not self.bot.is_closed():
-
-            message = await self.bot.wait_for('message', check=check)
+        if self.bot.user.mention in message.content \
+                and not message.content.startswith(self.bot.command_prefix):
 
             with open('cogs/Responses/mentions.json', 'r') as f:
                 mentions = json.load(f)['mentions']
@@ -63,59 +68,44 @@ class Responses(BasicCog):
             await self.send_typing_delay(message.channel)
             await message.channel.send(out)
 
-    async def hello_there(self):
+    @commands.Cog.listener(name='on_message')
+    async def hello_there(self, message):
         """Sends a picture if 'hello there' is in a message."""
-        await self.bot.wait_until_ready()
 
-        cooldown = timedelta(minutes=1)
-        last_time = datetime.now() - cooldown
+        if 'hello there' in message.content.lower() \
+                and not message.content.startswith(self.bot.command_prefix) \
+                and message.channel.name != 'general':
 
-        def check(message):
-            content = message.content
-            valid = 'hello there' in content.lower() and \
-                not content.startswith(self.bot.command_prefix) and\
-                message.channel.name != 'general'
-            return valid
+            dt = datetime.now() - self.hello_there_params['last_time']
 
-        while not self.bot.is_closed():
-
-            message = await self.bot.wait_for('message', check=check)
-            dt = datetime.now() - last_time
-
-            if dt > cooldown:
+            if dt > self.hello_there_params['cooldown']:
                 pics = glob('cogs/Responses/hellothere/*.*')
                 meme = discord.File(np.random.choice(pics))
                 await self.send_typing_delay(message.channel)
                 # await self.channel_msg.send(file=meme)
                 await message.channel.send(file=meme)
-                last_time = datetime.now()
+                self.hello_there_params['last_time'] = datetime.now()
 
-    async def they_said(self):
+    @commands.Cog.listener(name='on_message')
+    async def they_said(self, message):
         """Fun feature that replies the last message IN CAPS."""
-        await self.bot.wait_until_ready()
 
         whats = ('what', 'whut', 'whot', 'wat', 'wut', 'wot')
-        last_author = None
-        last_message = None
-        cooldown = timedelta(minutes=1)
-        last_time = datetime.now() - cooldown
-        chained = False
 
-        def check(message):
-            valid = message.content.lower() in whats and\
-                message.channel.name != 'general'
-            return valid
 
-        while not self.bot.is_closed():
+        if message.content.lower() in whats \
+                and message.channel.name != 'general':
 
-            message = await self.bot.wait_for('message', check=check)
+
             author = message.author
             channel = message.channel
-            dt = datetime.now() - last_time
+            dt = datetime.now() - self.they_said_params['last_time']
 
-            if author == last_author and dt < cooldown:
-                last_time = datetime.now()
-                continue  # don't let people spam wat in succession
+            if author == self.they_said_params['last_author'] \
+                    and dt < self.they_said_params['cooldown']:
+
+                self.they_said_params['last_time'] = datetime.now()
+                return  # don't let people spam wat in succession
 
             i = 0
             async for m in channel.history(limit=3):
@@ -135,32 +125,32 @@ class Responses(BasicCog):
                     # don't select a message with mentions
                     # don't select a message with role mentions
                     # don't select a message that mentionned @everyone
-                    is_valid = m.author != author and \
-                        not m.author.bot and \
-                        m.content.lower() not in whats and \
-                        len(m.embeds) == 0 and \
-                        len(m.attachments) == 0 and \
-                        len(m.mentions) == 0 and \
-                        len(m.role_mentions) == 0 and \
-                        not m.mention_everyone
+                    is_valid = m.author != author \
+                        and not m.author.bot \
+                        and m.content.lower() not in whats \
+                        and len(m.embeds) == 0 \
+                        and len(m.attachments) == 0 \
+                        and len(m.mentions) == 0 \
+                        and len(m.role_mentions) == 0 \
+                        and not m.mention_everyone
 
                     # if a second whats is sent, only repeat the same message
                     # if it follows a whats directly
-                    chained = is_valid
+                    self.they_said_params['chained'] = is_valid
 
-                elif i == 3 and chained:
+                elif i == 3 and self.they_said_params['chained']:
                     if m.content.lower() in whats:
                         is_valid = True
-                        m = last_message
+                        m = self.they_said_params['last_message']
                     else:
                         # don't send anything
                         # print('stop looking for message')
-                        break
+                        return
 
                 if is_valid:
-                    last_author = author
-                    last_message = m
-                    last_time = datetime.now()
+                    self.they_said_params['last_author'] = author
+                    self.they_said_params['last_message'] = m
+                    self.they_said_params['last_time'] = datetime.now()
                     out_content = m.content.upper()
 
                     if message.content == message.content.upper():
@@ -172,7 +162,7 @@ class Responses(BasicCog):
                     await self.send_typing_delay(channel)
                     await channel.send(out_str)
                     # print('Sent they_said at i={} ({})'.format(i, out_content))
-                    break
+                    return
 
     async def send_typing_delay(self, channel):
         r = np.random.rand()  # [0, 1)
