40ebf3edd2a706a87a8ede3279e4c6e61176ecd7
François Hardy <snaptraks@gmail.com>
2019-11-06 21:52:59 -0500

    discord.py version exists

--- cogs/utils/formats.py
+++ cogs/utils/formats.py
@@ -1,10 +1,2 @@
-def escape(string):
-    return string.translate(str.maketrans({'_': r'\_',
-                                           '*': r'\*',
-                                           '~': r'~',
-                                           '`': r'\`',
-                                           '|': r'\|',
-                                           '\\': r'\\',
-                                           }))
 
 zero_width_space = '\U0000200B'
