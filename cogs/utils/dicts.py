c81a53c313a221b2c6283ca7615761cda4500b02
François Hardy <snaptraks@gmail.com>
2020-03-12 13:57:47 -0400

    Better AttrDict implementation

--- cogs/utils/dicts.py
+++ cogs/utils/dicts.py
@@ -3,13 +3,37 @@ from collections import Counter
 
 class AttrDict(dict):
     """Dictionary subclass whose entries can be accessed by attributes
-    (as well as normally).
-    https://stackoverflow.com/questions/38034377/object-like-attribute-access-for-nested-dictionary
+    (as well as normally). Most of the code is from stackoverflow.
     """
+    def __getattr__(self, name):
+        if name in self:
+            return self[name]
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.__dict__ = self
+    def __setattr__(self, name, value):
+        self[name] = self.from_nested_dict(value)
+
+    def __delattr__(self, name):
+        if name in self:
+            del self[name]
+
+    # to allow pickling
+    def __getstate__(self):
+        return self.__dict__
+
+    # to allow pickling
+    def __setstate__(self, d):
+        self.__dict__.update(d)
+
+    def to_dict(self):
+        d = {}
+        for name in self:
+            if isinstance(self[name], self.__class__):
+                d[name] = self[name].to_dict()
+
+            else:
+                d[name] = self[name]
+
+        return d
 
     @classmethod
     def from_nested_dict(cls, data):
