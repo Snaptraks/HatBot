8cbcb415f81a09f462b6a7c34057b3febeec1156
François Hardy <snaptraks@gmail.com>
2020-06-08 17:26:18 -0400

    Create some DB tests

--- migrate_fishing.py
+++ migrate_fishing.py
@@ -203,17 +203,3 @@ for id, entry in fish_data.items():
             for _ in range(amount):
                 insert_fish(db, fish_dict)
 
-with db:
-    c = db.execute('SELECT * FROM fishing_fish')
-    print(f'{len(c.fetchall())} total fish in DB')
-
-    c = db.execute('SELECT * FROM fishing_fish GROUP BY user_id')
-    print(f'{len(c.fetchall())} total users in DB')
-
-    for size in FISH_SPECIES.keys():
-        c = db.execute('SELECT * FROM fishing_fish WHERE size = ?', (size,))
-        print(f'{len(c.fetchall())} {size} fish')
-
-    c = db.execute('SELECT * FROM fishing_fish WHERE user_id = 337266376941240320')
-    print(len(c.fetchall()))
-    print(fish_data[337266376941240320]['total_caught'])
--- test_migrate_fishing.py
+++ test_migrate_fishing.py
@@ -5,3 +5,19 @@ import pickle
 
 db = sqlite3.connect('HatBot.db')
 db.row_factory = sqlite3.Row
+
+
+with db:
+    c = db.execute('SELECT * FROM fishing_fish')
+    print(f'{len(c.fetchall())} total fish in DB')
+
+    c = db.execute('SELECT * FROM fishing_fish GROUP BY user_id')
+    print(f'{len(c.fetchall())} total users in DB')
+
+    for size in FISH_SPECIES.keys():
+        c = db.execute('SELECT * FROM fishing_fish WHERE size = ?', (size,))
+        print(f'{len(c.fetchall())} {size} fish')
+
+    c = db.execute('SELECT * FROM fishing_fish WHERE user_id = 337266376941240320')
+    print(len(c.fetchall()))
+    print(fish_data[337266376941240320]['total_caught'])
