80fe4beab5eeea1914849fa819b1e034b40caa4b
Snaptraks <snaptraks@gmail.com>
2020-12-07 19:44:35 -0500

    Rework utils.gifs

--- cogs/utils/gifs.py
+++ cogs/utils/gifs.py
@@ -1,36 +1,39 @@
+import random
 import config
-
-
-async def search_gif(http_session, query):
-    """Return a gif when searching for a specific query."""
-    return await _tenor_endpoint("search", http_session, query)
 
 
 async def random_gif(http_session, query):
     """Return a random gif related to the query."""
-    return await _tenor_endpoint("random", http_session, query)
+
+    limit = 10
+    params = {
+        "q": query,
+        "limit": limit,
+        "pos": limit * random.randint(0, 10),
+        "media_filter": "minimal",
+        "contentfilter": "high",
+        "locale": "en",
+    }
+    resp = await _tenor_endpoint(http_session, "search", params)
+    resp = resp['results']
+    random_gif = random.choice(resp)
+    gif_url = random_gif['media'][0]['gif']['url']
+
+    return gif_url
 
 
-async def _tenor_endpoint(endpoint, http_session, query):
+async def _tenor_endpoint(http_session, endpoint, params):
     """Get a gif from Tenor"""
 
-    query = query.split()
+    query = params["q"].split()
+    params["key"] = config.tenor_api_key
     if len(query) >= 1:
-        search_random = (
-            f"https://api.tenor.com/v1/{endpoint}?key={config.tenor_api_key}"
-            f"&q={query}&limit=1&media_filter=basic&contentfilter=low"
-        )
-        async with http_session.get(search_random) as resp:
+        async with http_session.get(f"https://api.tenor.com/v1/{endpoint}",
+                                    params=params) as resp:
             if resp.status == 200:
                 try:
                     json_resp = await resp.json()
-                    json_resp = json_resp['results']
-                    gif = json_resp[0]
-                    gif = gif.get('media')
-                    gif = gif[0]
-                    gif = gif.get('gif')
-                    gif = gif.get('url')
-                    return gif
-                except Exception as e:
+                    return json_resp
+                except Exception:
                     pass
     return ""
