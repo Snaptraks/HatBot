e099f11e06d310006d9b6d364fa66869c9e60bf8
Snaptraks <snaptraks@gmail.com>
2021-01-13 14:28:44 -0500

    Initial commit for WolframAlpha integration

---
+++ cogs/WolframAlpha/wolframalpha.py
@@ -0,0 +1,17 @@
+from discord.ext import commands
+import wolframalpha
+
+import config
+
+
+class WolframAlpha(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.wolfram_client = wolframalpha.Client(
+            config.wolfram_alpha_api,
+            session=self.bot.http_session,
+        )
+
+    @commands.command()
+    async def wolfram(self, ctx, *, input_):
+        res = await self.wolfram_client.query(input_)
