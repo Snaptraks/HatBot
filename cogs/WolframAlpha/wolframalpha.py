4a64925287dfe21ff1976102fa8e8d2544df027f
Snaptraks <snaptraks@gmail.com>
2021-01-13 17:34:01 -0500

    Drop everything and use simple API instead of full

--- cogs/WolframAlpha/wolframalpha.py
+++ cogs/WolframAlpha/wolframalpha.py
@@ -1,5 +1,6 @@
+import io
+import discord
 from discord.ext import commands
-import aiowolframalpha
 
 import config
 
@@ -7,11 +8,48 @@ import config
 class WolframAlpha(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.wolfram_client = aiowolframalpha.Client(
-            config.wolfram_alpha_api,
-            session=self.bot.http_session,
-        )
 
-    @commands.command()
-    async def wolfram(self, ctx, *, input_):
-        res = await self.wolfram_client.query(input_)
+    @commands.command(aliases=["wa"])
+    async def wolfram(self, ctx, *, query):
+        """Simple Wolfram|Alpha query command."""
+
+        async with ctx.typing():
+            result = await self.get_wolfram_query(query)
+            file = discord.File(result, filename="wolfram_alpha_result.png")
+            await ctx.reply(file=file)
+
+    @wolfram.error
+    async def wolfram_error(self, ctx, error):
+        """Error handler for the wolfram command."""
+
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.reply(error)
+
+        else:
+            raise error
+
+    async def get_wolfram_query(self, query):
+        """Function to send the query to Wolfram|Alpha and return the
+        generated image.
+        """
+        parameters = {
+            "i": query,
+            "appid": config.wolfram_alpha_api,
+            "layout": "labelbar",  # default: "divider"
+            "background": "36393F",  # default: "white"
+            "foreground": "white",  # default: "black"
+            "fontsize": 20,  # default: 14
+            "width": 600,  # default: 500
+            "units": "metric",  # default: location based
+        }
+
+        url = "http://api.wolframalpha.com/v1/simple"
+        async with self.bot.http_session.get(url, params=parameters) as resp:
+            if resp.status == 200:
+                result = io.BytesIO(await resp.content.read())
+                result.seek(0)
+
+                return result
+
+            else:
+                resp.raise_for_status()
