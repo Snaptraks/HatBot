46ae70243dabcf563d978a6b273da82cd476200c
Snaptraks <snaptraks@gmail.com>
2021-01-14 12:16:42 -0500

    Make simple query as subcommand

--- cogs/WolframAlpha/wolframalpha.py
+++ cogs/WolframAlpha/wolframalpha.py
@@ -9,16 +9,21 @@ class WolframAlpha(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(aliases=["wa"])
+    @commands.group(aliases=["wa"], invoke_without_command=True)
     async def wolfram(self, ctx, *, query):
-        """Simple Wolfram|Alpha query command."""
 
+    @wolfram.command(name="simple", aliases=["s"])
+    async def wolfram_simple(self, ctx, *, query):
+        """Simple Wolfram|Alpha query command.
+        Return the data in an already generated image from Wolfram itself.
+        """
         async with ctx.typing():
-            result = await self.get_wolfram_query(query)
+            result = await self.get_wolfram_simple_query(query)
             file = discord.File(result, filename="wolfram_alpha_result.png")
             await ctx.reply(file=file)
 
     @wolfram.error
+    @wolfram_simple.error
     async def wolfram_error(self, ctx, error):
         """Error handler for the wolfram command."""
 
@@ -28,7 +33,7 @@ class WolframAlpha(commands.Cog):
         else:
             raise error
 
-    async def get_wolfram_query(self, query):
+    async def get_wolfram_simple_query(self, query):
         """Function to send the query to Wolfram|Alpha and return the
         generated image.
         """
@@ -38,7 +43,7 @@ class WolframAlpha(commands.Cog):
             "layout": "labelbar",  # default: "divider"
             "background": "36393F",  # default: "white"
             "foreground": "white",  # default: "black"
-            "fontsize": 20,  # default: 14
+            "fontsize": 14,  # default: 14
             "width": 600,  # default: 500
             "units": "metric",  # default: location based
         }
