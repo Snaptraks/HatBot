34097b15ff6b191771ae89fb20b5f240836afda7
Snaptraks <snaptraks@gmail.com>
2023-08-08 18:09:38 -0400

    Update Announcements cog to v2.0

--- cogs/Announcements/announcements.py
+++ cogs/Announcements/announcements.py
@@ -1,18 +1,46 @@
 import asyncio
 import datetime
+from enum import Enum
+import logging
+from pathlib import Path
+import random
 
 import discord
+from discord import app_commands
 from discord.ext import commands, tasks
 
-from ..utils.cogs import BasicCog
-from . import menus
+from snapcogs import Bot
+from snapcogs.utils import relative_dt
+from snapcogs.utils.db import read_sql_query
+from snapcogs.utils.views import confirm_prompt
 
 
-def get_next_birthday(date):
+LOGGER = logging.getLogger(__name__)
+SQL = Path(__file__).parent / "sql"
+
+
+class Month(Enum):
+    January = 1
+    Febuary = 2
+    March = 3
+    April = 4
+    May = 5
+    June = 6
+    July = 7
+    August = 8
+    September = 9
+    October = 10
+    November = 11
+    December = 12
+
+
+def get_next_birthday(date: datetime.date) -> datetime.date:
     """Return a date object for the next birthday."""
 
-    now = datetime.date.today()
-    bday = date.replace(year=now.year)
+    now = discord.utils.utcnow()
+    bday = datetime.datetime(
+        year=now.year, month=date.month, day=date.day, hour=12, tzinfo=datetime.UTC
+    )
 
     if (bday - now).total_seconds() < 0:
         bday = bday.replace(year=bday.year + 1)
@@ -20,285 +48,217 @@ def get_next_birthday(date):
     return bday
 
 
-def time_until_birthday(date):
-    """Return the time until the date.
-    Is always positive, if the current year's date is passed already,
-    checks for next year.
-    """
-    time_until_bday = get_next_birthday(date) - datetime.date.today()
-    return time_until_bday
+class Announcements(commands.Cog):
+    birthday = app_commands.Group(
+        name="birthday", description="Save and celebrate server members' birthday!"
+    )
 
+    def __init__(self, bot: Bot):
+        self.bot = bot
 
-class AlreadyRegistered(Exception):
-    """Exception raised when a user already has a birthday registered."""
-
-    def __init__(self, date):
-        self.date = date
-
-
-class Announcements(BasicCog):
-    def __init__(self, bot):
-        super().__init__(bot)
-
-        self._create_tables.start()
+    async def cog_load(self):
+        await self._create_tables()
         self.birthday_announcement.start()
 
-    def cog_unload(self):
-        super().cog_unload()
+    async def cog_unload(self):
         self.birthday_announcement.cancel()
 
-    @commands.Cog.listener(name='on_member_join')
-    async def nth_member(self, member):
-        """Announce when a new member is the nth member of the guild.
-        Something like the (n * 50)th member sounds good.
-        """
-        each_n = 50
-        member_count = len([m for m in member.guild.members if not m.bot])
-
-        if member_count % each_n == 0:
-            system_channel = member.guild.system_channel
-            content = (
-                f":tada: {member.display_name} is our {member_count}th "
-                f"member! Welcome to the {member.guild.name} server!"
-            )
-            await system_channel.send(content)
-
-    @commands.Cog.listener(name='on_message')
-    async def birthday_confetti(self, message):
-        """If a birthday is active, add birthday reactions to messages
-        containing 'happy birthday'.
-        """
-        if message.guild is not None \
-                and message.channel == message.guild.system_channel:
-            if self.birthday_active:
-                # Add a reaction to people wishing happy birthday
-                trigger_words = [
-                    "bday",
-                    "birthday",
-                    "birfday",  # because people are silly
-                    "happy",
-                ]
-                if any(word in message.content.lower()
-                        for word in trigger_words):
-                    await message.add_reaction('\U0001F389')
-
-    # DISCORD.PY > 1.3.0 ONLY
-    # @tasks.loop(time=datetime.time(hour=0))
-    @tasks.loop(hours=24)
+    # @tasks.loop(time=datetime.time(hour=0, tzinfo=datetime.UTC))
+    @tasks.loop(count=1)
     async def birthday_announcement(self):
         """Accounce the birthday of a member.
         Birthdays need to be registered by the member beforehand
         with the command `!birthday register <DD/MM/YYYY>`.
         """
         birthdays = await self._get_today_birthday()
+        LOGGER.debug(f"Found {len(birthdays)} for today")
 
         if len(birthdays) != 0:
-            self.birthday_active = True
-
             for bday in birthdays:
-                member = self.guild.get_member(bday['user_id'])
-                asyncio.create_task(self.birthday_task(member))
-
-        else:
-            self.birthday_active = False
-
-    async def birthday_task(self, member):
-        msg = await self.guild.system_channel.send(
-            f":birthday: It is the birthday of {member.mention} today! "
-            "Let's all wish them a nice day!"
-        )
-        await msg.add_reaction('\U0001F389')
-        await member.add_roles(self.birthday_role, reason="Birthday!")
-        await asyncio.sleep(24 * 3600)  # 24 hours
-        await member.remove_roles(self.birthday_role)
+                guild = self.bot.get_guild(bday["guild_id"])
+                member = guild.get_member(bday["user_id"])
+                if member:
+                    # if we bring back the Birthday role,
+                    # this needs to be called as a task
+                    asyncio.create_task(self.birthday_task(member))
 
     @birthday_announcement.before_loop
     async def birthday_announcement_before(self):
         await self.bot.wait_until_ready()
-        self.birthday_active = False
-        # Hatventures Community
-        self.guild = self.bot.get_guild(308049114187431936)
-        # Bot Testing Server
-        # self.guild = self.bot.get_guild(588171715960635393)
 
-        self.birthday_role = discord.utils.get(
-            self.guild.roles,
-            name="Birthday Hat",
+    async def birthday_task(self, member: discord.Member):
+        """Task to send the birthday Embed to the member's guild's system channel."""
+
+        LOGGER.debug(f"Celebrating {member}'s birthday")
+        embed = discord.Embed(
+            description=(
+                f"# Happy Birthday {member.mention}!\n"
+                f"It is a very special day! Let's all wish them a happy birthday!"
+            ),
+            color=0xFFD700,
         )
-        # ----temp fix until tasks rework comes out----
-        t = datetime.datetime.utcnow().replace(hour=12, minute=0)
-        if t < datetime.datetime.utcnow():
-            t += datetime.timedelta(days=1)
-        await discord.utils.sleep_until(t)
-        # --------------------------------------
+        embed.set_thumbnail(url=member.display_avatar.url)
+        embed.set_footer(
+            text=(
+                "Want to celebrate your birthday too? "
+                "Register with /birthday register."
+            ),
+            icon_url="https://em-content.zobj.net/thumbs/160/twitter/351/information_2139-fe0f.png",  # noqa
+        )
+        message = await member.guild.system_channel.send(embed=embed)
+        reactions = [
+            "\U0001f973",
+            "\U0001f382",
+            "\U0001f381",
+            "\U0001f389",
+        ]
+        random.shuffle(reactions)
 
-    @commands.group(aliases=["bday"])
-    async def birthday(self, ctx):
-        """Command group to register a birthday.
-        Check the current registered date, if any.
-        """
-        if ctx.invoked_subcommand is None:
-            row = await self._get_member_birthday(ctx.author)
-
-            if row is not None:
-                bday = row['birthday']
-                time_until_bday = time_until_birthday(bday)
-                content = (
-                    f"Your birthday is **{bday.strftime('%d of %B')}**. "
-                    f"See you in {time_until_bday.days} day(s)!"
-                )
-
-            else:
-                content = (
-                    "You can register your birthday with "
-                    "`!birthday register <DD/MM/YYYY>` in a private "
-                    "message with me."
-                )
-
-            await ctx.reply(content)
+        for reaction in reactions:
+            await message.add_reaction(reaction)
+        # await member.add_roles(self.birthday_role, reason="Birthday!")
+        # await asyncio.sleep(24 * 3600)  # 24 hours
+        # await member.remove_roles(self.birthday_role)
 
     @birthday.command(name="register")
-    @commands.dm_only()
-    async def birthday_register(self, ctx, date):
-        """Register your birthday.
-        Format DD/MM/YYYY. Only works in Private Message with the bot.
-        """
-        already_registered = await self._is_already_registered(ctx.author)
-        if already_registered:
-            date = await self._get_member_birthday(ctx.author)
-            raise AlreadyRegistered(date['birthday'])
+    @app_commands.describe(month="Month of the year", day="Day of the month (1-31)")
+    async def birthday_register(
+        self,
+        interaction: discord.Interaction,
+        month: Month,
+        day: app_commands.Range[int, 1, 31],
+    ):
+        """Register your birthday in this server."""
 
-        # will raise ValueError if there is a non-int
-        date = [int(x) for x in date.split('/')]
+        # save year as a leap year. no need for user year of birth!
+        birthday_date = datetime.date(4, month.value, day)
+        next_birthday = get_next_birthday(birthday_date)
+        LOGGER.debug(f"{interaction.user} registered birthday for {birthday_date}")
 
-        if len(date) != 3:
-            raise ValueError
+        await self._save_birthday(interaction.user, birthday_date)
 
-        bday = datetime.date(
-            day=date[0],
-            month=date[1],
-            year=date[2],
+        await interaction.response.send_message(
+            f"Saved your birthday as {month.name} {day}. "
+            f"See you {relative_dt(next_birthday)}!",
+            ephemeral=True,
         )
 
-        confirm = await menus.ConfirmBirthday(
-            f"Is your birthday **{bday.strftime('%d of %B')}**?").prompt(ctx)
-
-        if confirm:  # yes
-            await self._save_birthday(ctx.author, bday)
-
-            time_until_bday = time_until_birthday(bday)
-            content = (
-                "I saved your birthday! See you in "
-                f"{time_until_bday.days} day(s)!"
-            )
-
-        else:  # no
-            content = "To enter again, just send the command again!"
-
-        await ctx.reply(content)
-
     @birthday_register.error
-    async def birthday_register_error(self, ctx, error):
-        """In the case of an error, send a helpful message."""
+    async def birthday_register_error(
+        self, interaction: discord.Interaction, error: app_commands.AppCommandError
+    ):
+        """Error handler for the birthday register subcommand."""
 
-        if isinstance(error, commands.PrivateMessageOnly):
-            await ctx.author.send("You can only register a birthday in DMs.")
+        error = getattr(error, "original", error)
 
-        elif isinstance(error, commands.MissingRequiredArgument) \
-                or isinstance(error.original, ValueError):
-
-            await ctx.reply(
-                "Please enter your birthday in a "
-                "`DD/MM/YYYY` format."
-            )
-
-        elif isinstance(error.original, AlreadyRegistered):
-            await ctx.reply(
-                "You already have a birthday registered "
-                f"(**{error.original.date.strftime('%d of %B')}**)! "
-                f"Contact {self.bot.owner.mention} to change it."
+        if isinstance(error, ValueError):
+            month = interaction.namespace.month
+            day = interaction.namespace.day
+            LOGGER.debug(f"{day} is not in {month}")
+            await interaction.response.send_message(
+                f"Day {day} is out of range for month {month}",
+                ephemeral=True,
             )
 
         else:
-            raise error
+            interaction.extras["error_handled"] = False
 
     @birthday.command(name="celebrate")
-    @commands.has_permissions(mention_everyone=True)
-    async def birthday_celebrate(self, ctx, member: discord.Member):
+    @app_commands.checks.has_permissions(mention_everyone=True)
+    @app_commands.describe(member="The person whose birthday is today!")
+    async def birthday_celebrate(
+        self, interaction: discord.Interaction, member: discord.Member
+    ):
         """Celebrate a member's birthday!"""
 
-        self.birthday_active = True
         asyncio.create_task(self.birthday_task(member))
+        await interaction.response.send_message(
+            f"Thank you for celebrating {member.mention}!", ephemeral=True
+        )
 
-        already_registered = await self._is_already_registered(member)
+    @birthday_celebrate.error
+    async def birthday_celebrate_error(
+        self, interaction: discord.Interaction, error: app_commands.AppCommandError
+    ):
+        """Error handler for the birthday celebrate subcommand."""
 
-        if not already_registered:
-            # send a message to member to ask if they would like to register
-            today = datetime.date.today().strftime('%d/%m/%Y')
-            content = (
-                "It seems it is your birthday today! Sadly I did not know "
-                "and someone just told me. If you would like me to remember "
-                "for next time please enter the command "
-                f"`!birthday register {today}`! And happy birthday!"
+        error = getattr(error, "original", error)
+
+        if isinstance(error, app_commands.MissingPermissions):
+            await interaction.response.send_message(
+                "You are missing permissions for this command "
+                f"({', '.join(error.missing_permissions)})"
             )
-            await member.send(content)
+
+        else:
+            interaction.extras["error_handled"] = False
 
     @birthday.command(name="delete")
-    @commands.is_owner()
-    async def birthday_delete(self, ctx, user: discord.User):
-        """Delete a registered birthday.
-        Only use it if someone entered the wrong date even after
-        the confirmation message.
-        """
-        await self._delete_birthday(user)
-        await ctx.reply(f"Successfully removed birthday for {user}")
+    async def birthday_delete(self, interaction: discord.Interaction):
+        """Delete a registered birthday."""
 
-    @tasks.loop(count=1)
+        birthday_date = await self._get_member_birthday(interaction.user)
+        LOGGER.debug(f"Retreived birthday is {birthday_date}")
+
+        if birthday_date is None:
+            await interaction.response.send_message(
+                "You did not register a birthday here, so nothing to delete!",
+                ephemeral=True,
+            )
+            return
+
+        confirm = await confirm_prompt(
+            interaction,
+            "Are you sure you want to delete your birthday? "
+            "You can register it again later.",
+        )
+
+        if confirm.value is None:
+            LOGGER.debug("Confirmation timed out.")
+            await interaction.followup.send("Confirmation timed out.", ephemeral=True)
+            return
+
+        if confirm.value:
+            await self._delete_birthday(interaction.user)
+            content = "Deleting your birthday!"
+
+        else:
+            next_birthday = get_next_birthday(birthday_date)
+            content = f"Keeping your birthday. See you in {relative_dt(next_birthday)}!"
+
+        await confirm.interaction.response.send_message(content, ephemeral=True)
+
     async def _create_tables(self):
         """Create the necessary DB tables if they do not exist."""
 
-        await self.bot.db.execute(
-            """
-            CREATE TABLE IF NOT EXISTS announcements_birthday(
-                user_id  INTEGER PRIMARY KEY,
-                birthday DATE
-                )
-            """
-        )
+        await self.bot.db.execute(read_sql_query(SQL / "create_tables.sql"))
 
         await self.bot.db.commit()
 
-    async def _get_member_birthday(self, member):
+    async def _get_member_birthday(
+        self, member: discord.Member
+    ) -> datetime.date | None:
         """Get a member's birthday."""
 
         async with self.bot.db.execute(
-                """
-                SELECT *
-                  FROM announcements_birthday
-                 WHERE user_id = :user_id
-                """,
-                {'user_id': member.id}
+            read_sql_query(SQL / "get_member_birthday.sql"),
+            dict(user_id=member.id, guild_id=member.guild.id),
         ) as c:
             row = await c.fetchone()
 
-        return row
+        return row["birthday"] if row is not None else None
 
     async def _get_today_birthday(self):
         """Return a list of today's birthdays.
         The list is empty is there is none.
         """
-        async with self.bot.db.execute(
-                """
-                SELECT *
-                  FROM announcements_birthday
-                """
-        ) as c:
+        async with self.bot.db.execute("SELECT * FROM announcements_birthday") as c:
             rows = await c.fetchall()
 
         birthdays = []
         today = datetime.date.today()
         for row in rows:
-            date = row['birthday']
+            date = row["birthday"]
             if (date.day, date.month) == (today.day, today.month):
                 birthdays.append(row)
 
@@ -308,26 +268,19 @@ class Announcements(BasicCog):
         """Verify if a member has registered a birthday already."""
 
         async with self.bot.db.execute(
-                """
-                SELECT EXISTS (SELECT 1
-                                 FROM announcements_birthday
-                                WHERE user_id = :user_id)
-                """,
-                {'user_id': member.id}
+            read_sql_query(SQL / "is_already_registered.sql"),
+            dict(user_id=member.id, guild_id=member.guild.id),
         ) as c:
             row = await c.fetchone()
 
         return bool(row[0])
 
-    async def _save_birthday(self, member, birthday):
+    async def _save_birthday(self, member: discord.Member, birthday: datetime.date):
         """Save the birthday to the database."""
 
         await self.bot.db.execute(
-            """
-            INSERT INTO announcements_birthday
-            VALUES (:user_id, :birthday)
-            """,
-            {'user_id': member.id, 'birthday': birthday}
+            read_sql_query(SQL / "save_birthday.sql"),
+            dict(user_id=member.id, guild_id=member.guild.id, birthday=birthday),
         )
         await self.bot.db.commit()
 
@@ -335,10 +288,7 @@ class Announcements(BasicCog):
         """Remove the member's birthday from the database."""
 
         await self.bot.db.execute(
-            """
-            DELETE FROM announcements_birthday
-             WHERE user_id = :user_id
-            """,
-            {'user_id': member.id}
+            read_sql_query(SQL / "delete_birthday.sql"),
+            dict(user_id=member.id, guild_id=member.guild.id),
         )
         await self.bot.db.commit()
